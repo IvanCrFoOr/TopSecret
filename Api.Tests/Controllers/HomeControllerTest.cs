53507eff315ba11dd632983f49e011db02a3f7f8
IvanCrFoOr <69528049+IvanCrFoOr@users.noreply.github.com>
2021-07-26 16:54:45 -0500

    TestUnit

--- Api.Tests/Controllers/HomeControllerTest.cs
+++ Api.Tests/Controllers/HomeControllerTest.cs
@@ -2,24 +2,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Api;
 using Api.Controllers;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace Api.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class SecretControllerTest
     {
         [TestMethod]
         public void Index()
         {
             // Disponer
-            HomeController controller = new HomeController();
+            TopSecretController controller = new TopSecretController();
 
             // Actuar
-            ViewResult result = controller.Index() as ViewResult;
+            var input = new List<Api.Models.Satelites>();
+            input.Add(new Models.Satelites { Distance = 100.0, Name = "kenobi", Message = new string[] { "este", "es", "", "", "mensaje" } });
+            input.Add(new Models.Satelites { Distance = 115.5, Name = "skywalker", Message = new string[] { "","es", "","","secreto" } });
+            input.Add(new Models.Satelites { Distance = 142.7, Name = "soto", Message = new string[] { "este", "", "un", "", "" } });
+            IEnumerable<Api.Models.Satelites> satelites = input as IEnumerable<Api.Models.Satelites>;
+            var result = controller.TopSecret(satelites);
 
             // Declarar
             Assert.IsNotNull(result);
-            Assert.AreEqual("Home Page", result.ViewBag.Title);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            
         }
     }
 }
